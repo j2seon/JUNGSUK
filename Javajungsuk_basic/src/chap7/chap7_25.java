ec1f1a0893d4b2531a10909769698916b88ceeca
Ju Jinseon <97584505+j2seon@users.noreply.github.com>
2022-05-03 00:19:53 +0900

    2022.05.03

---
+++ Javajungsuk_basic/src/chap7/chap7_25.java
@@ -0,0 +1,45 @@
+package chap7;
+
+
+interface Parseable{
+	public abstract void parse(String fileName); //구문작업을 수행한다.
+}
+
+class ParserManager{
+	public static Parseable getParser(String type) {
+		if(type.equals("XML")) {
+			return new XMLparser();
+		}else {
+			Parseable p = new HTMLparser();
+			return p;
+		}
+	}
+} 
+
+class XMLparser implements Parseable{//xml로 변환되는 클래스
+
+	public void parse(String fileName) {
+		System.out.println(fileName +  " - XML parsing completed");
+	}
+}
+
+class HTMLparser implements Parseable{ //Html로 변환되는 
+	public void parse(String fileName) {
+		System.out.println(fileName + " - HTML parsing completed");
+	}
+}
+
+public class chap7_25 {
+	public static void main(String[] args) {
+		Parseable p = ParserManager.getParser("XML"); // ParserManager의 getParser의 반환 타입이 인터페이스룰 구현하는 클래스!
+						// new XMLparser과 같은의미이다.
+		p.parse("document.xml");
+		p = ParserManager.getParser("HTML");
+		p.parse("document.Html");
+	
+		
+		
+		
+	}
+
+}
